b83a250f087d2d9536020b65aefbfd79d2e20210
AdamDono <111118088+AdamDono@users.noreply.github.com>
2023-06-06 10:29:55 +0200

    account

---
+++ Login Firebase/CreateAccountViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CreateAccountViewController.swift
+//  Login Firebase
+//
+//  Created by DA M1 MAC 89 on 2023/06/06.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
